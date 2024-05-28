bfd4c6a2dd645c7fc872b3a2555cdf15ec267712
haytam3719 <haytam.elmoufti@gmail.com>
2024-05-28 18:56:52 +0200

    Dark Mode

--- app/src/main/java/com/example/project/Apparence.kt
+++ app/src/main/java/com/example/project/Apparence.kt
@@ -4,27 +4,54 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
 import com.example.project.databinding.ApparenceBinding
 import com.google.android.material.appbar.MaterialToolbar
 
 class Apparence : Fragment(){
 
+    private var _binding: ApparenceBinding? = null
+    private val binding get() = _binding!!
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.checkbox1.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                binding.checkbox2.isChecked = false
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
+
+        binding.checkbox2.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                binding.checkbox1.isChecked = false
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            }
+        }
+
+
+
     }
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val binding = ApparenceBinding.inflate(inflater, container, false)
+        _binding = ApparenceBinding.inflate(inflater, container, false)
         val topAppBar: MaterialToolbar = binding.topAppBar
 
         topAppBar.setNavigationOnClickListener {
             requireActivity().onBackPressed()
         }
         return binding.root
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
